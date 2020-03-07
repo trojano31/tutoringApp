571b12d9b8abc48330dc5b50974cd33b8ae54fce
jakub.m <jakub1mrzyglod@gmail.com>
2020-03-07 10:30:38 +0100

    add sample to use base model

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { User } from './entity/user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get('/:id')
+  async getOneById (@Param('id') id: string) {
+    return await User.findByIdOrThrow(id);
+  } 
+}
