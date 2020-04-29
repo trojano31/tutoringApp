ea73fb2dc38626d80bf8b07f4e145ec13669dee5
Damian Obajtek <damian.obajtek@gmail.com>
2020-04-29 19:15:22 +0200

    wprowadizlem react-hook-forms ale cos nie bangla, poprawie

--- frontend/src/views/signup.js
+++ frontend/src/views/signup.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useLazyQuery, useMutation } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/react-hooks";
+import { useForm } from "react-hook-form";
 import gql from "graphql-tag";
-import { Input, Button } from "semantic-ui-react";
+import { Input, Button, Form } from "semantic-ui-react";
 import cogoToast from "cogo-toast";
 
 const ADD_USER = gql`
@@ -22,20 +23,92 @@ export const SignUp = () => {
   const [hashedPwd, setHashedPwd] = useState(null);
   const [addUser] = useMutation(ADD_USER);
 
-  // useEffect(() => {
-  //   console.log("data", data);
-  // }, [data]);
+  useEffect(() => {
+    register({ name: "email" }, { required: true });
+    register({ name: "firstName" }, { required: true });
+    register({ name: "lastName" }, { required: true });
+    register({ name: "password" }, { required: true });
+  }, []);
+
+  const { register, errors, handleSubmit, setValue, triggerValidation } = useForm();
+  const onSubmit = (data, e) => {
+    console.log("Submit event", e);
+    alert(JSON.stringify(data));
+  };
 
   return (
     <div>
-      <div>
-        <h3>creating user</h3>
-        <Input onChange={(e) => setEmail(e.target.value)} />
-        <Input onChange={(e) => setFirstName(e.target.value)} />
-        <Input onChange={(e) => setLastName(e.target.value)} />
-        <Input onChange={(e) => setHashedPwd(e.target.value)} />
-        <Button onClick={handleSignupClick}>sign up</Button>
-      </div>
+      <h3>Rejestracja</h3>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group widths="equal">
+          <Form.Input
+            fluid
+            id="form-input-control-error-email"
+            control={Input}
+            placeholder="joe@schmoe.com"
+            name="email"
+            onChange={async (e, { name, value }) => {
+              setEmail(e.target.value);
+              setValue(name, value);
+              await triggerValidation({ name });
+            }}
+            ref={register({
+              required: true,
+              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
+            error={errors.email ? true : false}
+          />
+
+          <Form.Input
+            fluid
+            id="form-input-control-first-name"
+            control={Input}
+            placeholder="First name"
+            name="firstName"
+            onChange={async (e, { name, value }) => {
+              setFirstName(e.target.value);
+              setValue(name, value);
+              await triggerValidation({ name });
+            }}
+            ref={register({ required: true, maxLength: 80 })}
+            error={errors.firstName ? true : false}
+          />
+
+          <Form.Input
+            fluid
+            id="form-input-control-last-name"
+            control={Input}
+            placeholder="Last name"
+            name="lastName"
+            onChange={async (e, { name, value }) => {
+              setLastName(e.target.value);
+              setValue(name, value);
+              await triggerValidation({ name });
+            }}
+            ref={register({ required: true })}
+            error={errors.lastName ? true : false}
+          />
+
+          <Form.Input
+            fluid
+            id="form-input=control-password"
+            control={Input}
+            placeholder="Hasło"
+            name="password"
+            type="password"
+            onChange={async (e, { name, value }) => {
+              setHashedPwd(e.target.value);
+              setValue(name, value);
+              await triggerValidation({ name });
+            }}
+            ref={register({ required: true })}
+            error={errors.password ? true : false}
+          />
+        </Form.Group>
+        <Button type="submit" primary onClick={handleSignupClick}>
+          Zarejestruj się
+        </Button>
+      </Form>
     </div>
   );
 
