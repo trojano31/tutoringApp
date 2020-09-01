4e7eb110f37c35600fa486df4b3bd53e7202ba1c
Damian Obajtek <damian.obajtek@gmail.com>
2020-09-01 19:48:23 +0200

    useless warnings

--- frontend/src/views/main.js
+++ frontend/src/views/main.js
@@ -1,32 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useMutation } from "@apollo/react-hooks";
+import React from "react";
 import gql from "graphql-tag";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { useForm } from "react-hook-form";
-import cogoToast from "cogo-toast";
-import { LOGIN } from "../mutations";
-import { Redirect } from "react-router-dom";
 import { MainAppBar } from "./components/appbar";
-
-const ADD_USER = gql`
-  mutation createUser($user: UserInput!) {
-    createUser(user: $user) {
-      email
-      firstName
-      lastName
-    }
-  }
-`;
 
 const useStyles = makeStyles((theme) => ({
   root: {
