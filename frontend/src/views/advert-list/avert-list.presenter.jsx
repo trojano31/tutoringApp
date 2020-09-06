28523369da86b7204cfc2ca100805e62fd718b29
jakub.m <jakub1mrzyglod@gmail.com>
2020-09-06 18:49:25 +0200

    add filter form to presenter component

--- frontend/src/views/advert-list/avert-list.presenter.jsx
+++ frontend/src/views/advert-list/avert-list.presenter.jsx
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid/Grid';
 import { AdvertListItem } from './advert-list-item';
 import { Typography } from '@material-ui/core';
 import { MainAppBar } from '../components/appbar';
+import { AdvertListFilterForm } from './advert-list-filter-form';
 
 export const AdvertListPresenter = ({ adverts }) => (
   <>
@@ -10,8 +11,13 @@ export const AdvertListPresenter = ({ adverts }) => (
     <Typography gutterBottom variant='h5' component='h2' color='primary' >
       Znalezione ogłoszenia
     </Typography>
-    <Grid container spacing={5}>
-      {adverts.map(AdvertListItem)}
+    <Grid container spacing={3}>
+      <Grid item xs={3}>
+        <AdvertListFilterForm />
+      </Grid>
+      <Grid item container spacing={5} xs={9}>
+        {adverts.map(AdvertListItem)}
+      </Grid>
     </Grid>
   </>
 );
