1886bec467f69a908cd077091e2afb8e365b7223
jakub.m <jakub1mrzyglod@gmail.com>
2020-09-06 18:04:15 +0200

    finish basic list

--- frontend/src/views/advert-list/adverts.js
+++ frontend/src/views/advert-list/adverts.js
@@ -1,32 +1,43 @@
 export const adverts = [
-  { 
-    subject: { name: 'Matematyka'},
+  {
+    teacher: { name: 'Jan' },
+    subject: { name: 'Matematyka' },
     place: 0,
     price: 50,
-    time: 60
+    time: 60,
   },
-  { 
-    subject: { name: 'Matematyka'},
+  {
+    teacher: { name: 'Jan' },
+    subject: { name: 'Matematyka' },
     place: 0,
     price: 50,
-    time: 60
+    time: 60,
   },
-  { 
-    subject: { name: 'Matematyka'},
+  {
+    teacher: { name: 'Jan' },
+    subject: { name: 'Matematyka' },
     place: 0,
     price: 50,
-    time: 60
+    time: 60,
   },
-  { 
-    subject: { name: 'Matematyka'},
+  {
+    teacher: { name: 'Jan' },
+    subject: { name: 'Matematyka' },
     place: 0,
     price: 50,
-    time: 60
+    time: 60,
   },
-  { 
-    subject: { name: 'Matematyka'},
+  {
+    teacher: { name: 'Jan' },
+    subject: { name: 'Matematyka' },
     place: 0,
     price: 50,
-    time: 60
+    time: 60,
   },
-]
+];
+
+export const Place = {
+  [0]: 'On-line',
+  [1]: 'U nauczyciela',
+  [2]: 'U ucznia',
+}
--- frontend/src/views/advert-list/avert-list.presenter.jsx
+++ frontend/src/views/advert-list/avert-list.presenter.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid/Grid';
+import { AdvertListItem } from './advert-list-item';
+import { Typography } from '@material-ui/core';
+import { MainAppBar } from '../components/appbar';
 
-export const AdvertListPresenter = ({ adverts }) =>
-<Grid container>
-  {adverts.map(advert => <>{advert.price}</>)}
-</Grid>
+export const AdvertListPresenter = ({ adverts }) => (
+  <>
+    <MainAppBar />
+    <Typography gutterBottom variant='h5' component='h2' color='primary' >
+      Znalezione ogłoszenia
+    </Typography>
+    <Grid container spacing={5}>
+      {adverts.map(AdvertListItem)}
+    </Grid>
+  </>
+);
